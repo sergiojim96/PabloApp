4add195e6e300f48c12ecbd21176b8be2dc77f96
sergio <sergioajm2909@gmail.com>
2022-02-17 22:17:48 -0600

    procfile3

--- app.js
+++ app.js
@@ -3,16 +3,17 @@ const app = express();
 const path = require('path');
 const router = express.Router();
 
-router.get('/',function(req,res){
-  res.sendFile(path.join(__dirname+'/index.html'));
+//router.get('/',function(req,res){
+//  res.sendFile(path.join(__dirname+'/index.html'));
   //__dirname : It will resolve to your project folder.
-});
+//});
 
-router.get('/about',function(req,res){
-  res.sendFile(path.join(__dirname+'/about.html'));
-});
+//router.get('/about',function(req,res){
+ // res.sendFile(path.join(__dirname+'/about.html'));
+//});
 
 //add the router
-app.use('/', router);
+//app.use('/', router);
 app.use('/', express.static(__dirname + '/'));
+app.get('/', (req, res) => res.render('index'))
 app.listen(process.env.port || 5000);
