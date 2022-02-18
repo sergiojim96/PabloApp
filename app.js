25390c3a399f13f6e23014c1a89cbe7aa0c371e7
sergio <sergioajm2909@gmail.com>
2022-02-17 21:24:08 -0600

    commit

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const router = express.Router();
+
+router.get('/',function(req,res){
+  res.sendFile(path.join(__dirname+'/index.html'));
+  //__dirname : It will resolve to your project folder.
+});
+
+router.get('/about',function(req,res){
+  res.sendFile(path.join(__dirname+'/about.html'));
+});
+
+//add the router
+app.use('/', router);
+app.use('/', express.static(__dirname + '/'));
+app.listen(process.env.port || 3000);
